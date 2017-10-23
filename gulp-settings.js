4f283c9c3d147ce26e6a54c606982905bd08107b
Frederic KOLLER <koller.f@gmail.com>
2017-10-23 08:45:16 +0200

    add asset to copy

--- gulp-settings.js
+++ gulp-settings.js
@@ -1,6 +1,22 @@
 const settings = {
-    "src": "src_assets",
-    "dest": "webroot"
+    src: "src_assets",
+    dest: "webroot",
+
+    assets: {
+        js: [
+            './node_modules/foundation-sites/dist/js/foundation.js',
+            './node_modules/foundation-sites/dist/js/foundation.min.js',
+            './node_modules/jquery/dist/jquery.js',
+            './node_modules/jquery/dist/jquery.min.js',
+            './node_modules/what-input/dist/what-input.min.js',
+            './node_modules/what-input/dist/what-input.js',
+            './node_modules/lodash/lodash.js',
+            './node_modules/lodash/lodash.min.js'
+        ],
+        css: [
+            //'./node_modules/normalize.css/normalize.css'
+        ]
+    }
 }
 
 module.exports  = settings
