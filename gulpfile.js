ca49f9659b16d9120cc1f286a5d33d77cbd8eecd
Frederic KOLLER <koller.f@gmail.com>
2017-10-23 08:45:43 +0200

    remove assets

--- gulpfile.js
+++ gulpfile.js
@@ -40,21 +40,7 @@ if (env === 'prod') {
 
 
 gulp.task('copy-assets', function () {
-  var assets = {
-    js: [
-      './node_modules/foundation-sites/dist/js/foundation.js',
-      './node_modules/foundation-sites/dist/js/foundation.min.js',
-      './node_modules/jquery/dist/jquery.js',
-      './node_modules/jquery/dist/jquery.min.js',
-      './node_modules/what-input/dist/what-input.min.js',
-      './node_modules/what-input/dist/what-input.js',
-      './node_modules/lodash/lodash.js',
-      './node_modules/lodash/lodash.min.js'
-    ],
-    // css: ['./node_modules/normalize.css/normalize.css']
-  }
-
-  _(assets).forEach(function (assets, type) {
+  _(settings.assets).forEach(function (assets, type) {
     gulp.src(assets).pipe(gulp.dest(dest + type + '/vendor/'))
   })
 })
